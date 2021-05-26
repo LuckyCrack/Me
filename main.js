97eabfe6c8b98ebf1ec06e39290d7edb6bb13209
Vaibhav Nanda <vaibhavnanda2000@gmail.com>
2021-05-26 17:50:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var w = window.innerWidth;
+/*var w = window.innerWidth;
 var h = window.innerHeight;
 var container = document.getElementById("unityContainer");
 container.style.height = h + "px";
 container.style.width = w + "px";
+*/
