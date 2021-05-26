228fc367a08e3f60a56347ed54aaaaad041e7b59
Vaibhav Nanda <vaibhavnanda2000@gmail.com>
2021-05-26 19:57:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        unityInstance.SetFullscreen(1);
+    }
+    else if (e.keyCode == '40') {
+        unityInstance.SetFullscreen(1);
+    }
+    else if (e.keyCode == '37') {
+       unityInstance.SetFullscreen(1);
+    }
+    else if (e.keyCode == '39') {
+       unityInstance.SetFullscreen(1);
+    }
+    else
+    {
+      unityInstance.SetFullscreen(1);
+    }
+
+}
