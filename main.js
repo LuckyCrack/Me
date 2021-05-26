2b9ee7d5203dc30776d3f5e18ceee604595298fb
Vaibhav Nanda <vaibhavnanda2000@gmail.com>
2021-05-26 16:32:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-var elem = document.getElementsByClassName("webgl-content");
+
 if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i))
- {
-       window.onload = function()
-      {
-        if (elem.requestFullscreen)
-        {
-          elem.requestFullscreen();
-        } else if (elem.webkitRequestFullscreen)
-        { /* Safari */
-          elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen)
-        { /* IE11 */
-          elem.msRequestFullscreen();
-        }
-      }
-    }
+{
+  unityInstance.SetFullscreen(1)
+}
