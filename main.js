459522f1c7e6767329e3d698287b0d0c1716d41e
Vaibhav Nanda <vaibhavnanda2000@gmail.com>
2021-05-26 16:47:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 
-if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) != null)
+/*if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) != null)
 {
   unityInstance.SetFullscreen(1)
-}
+}*/
+window.onload = function() {
+  unityInstance.SetFullscreen(1)
+};
