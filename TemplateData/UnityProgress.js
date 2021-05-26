c226edde6434b40cbaea2c6082a788d529eed23e
Vaibhav Nanda <vaibhavnanda2000@gmail.com>
2021-05-26 19:40:22 +0530

    Update UnityProgress.js

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -22,7 +22,6 @@ function UnityProgress(unityInstance, progress) {
   if (progress == 1)
   {
     unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
-    unityInstance.SetFullscreen(1);
   }
     
 }
