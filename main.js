288c0354ac5ba91dea89b6b27dd2e4c8da998c55
Vaibhav Nanda <vaibhavnanda2000@gmail.com>
2021-05-26 18:05:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-/*var w = window.innerWidth;
-var h = window.innerHeight;
-var container = document.getElementById("unityContainer");
-container.style.height = h + "px";
-container.style.width = w + "px";
-*/
+if(
+  navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i) != null)
+{
+  var elem = document.getElementsByClassName("webgl-content");
+  if (elem.requestFullscreen) 
+  {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) 
+  { 
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) 
+  { 
+    elem.msRequestFullscreen();
+  }
+}
