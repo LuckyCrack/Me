faeb1672ab34997c50818d137fee5be41c773676
Vaibhav Nanda <vaibhavnanda2000@gmail.com>
2021-05-26 17:34:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-
-/*if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) != null)
-{
-  unityInstance.SetFullscreen(1)
-}*/
-var elem = document.getElementById("unityContainer");
-
-/* When the openFullscreen() function is executed, open the video in fullscreen.
-Note that we must include prefixes for different browsers, as they don't support the requestFullscreen property yet */
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
+var w = window.innerWidth;
+var h = window.innerHeight;
+var container = document.getElementById("unityContainer");
+container.style.height = h + "px";
+container.style.width = w + "px";
