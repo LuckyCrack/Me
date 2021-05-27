e9f39def553499661a7a1d29f6ca70eab01f91c6
Vaibhav Nanda <vaibhavnanda2000@gmail.com>
2021-05-27 14:26:28 +0530

    Add files via upload

---
+++ TemplateData/UnityProgress.js
@@ -0,0 +1,45 @@
+function UnityProgress(unityInstance, progress) {
+  if (!unityInstance.Module)
+    return;
+  if (!unityInstance.logo) {
+    unityInstance.logo = document.createElement("div");
+    unityInstance.logo.className = "logo " + unityInstance.Module.splashScreenStyle;
+    unityInstance.container.appendChild(unityInstance.logo);
+  }
+  if (!unityInstance.progress) {
+    unityInstance.progress = document.createElement("div");
+    unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
+    unityInstance.progress.empty = document.createElement("div");
+    unityInstance.progress.empty.className = "empty";
+    unityInstance.progress.appendChild(unityInstance.progress.empty);
+    unityInstance.progress.full = document.createElement("div");
+    unityInstance.progress.full.className = "full";
+    unityInstance.progress.appendChild(unityInstance.progress.full);
+    unityInstance.container.appendChild(unityInstance.progress);
+  }
+  unityInstance.progress.full.style.width = (100 * progress) + "%";
+  unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
+  if (progress == 1)
+  {
+    unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
+    var ele = document.getElementById("info");
+    ele.style.display = "block";
+  }
+
+}
+
+function click_btn()
+{
+  var ele = document.getElementById("info");
+  ele.style.display = "none";
+  unityInstance.SetFullscreen(1);
+}
+
+function keyisdown()
+{
+ if (event.keyCode == 13){
+   var ele = document.getElementById("info");
+   ele.style.display = "none";
+   unityInstance.SetFullscreen(1);
+ }
+}
