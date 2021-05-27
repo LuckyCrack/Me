ec56df27b6f8394fe5935edfb326d8a89c551ad0
Vaibhav Nanda <vaibhavnanda2000@gmail.com>
2021-05-27 13:51:03 +0530

    Add files via upload

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -6,7 +6,7 @@ function UnityProgress(unityInstance, progress) {
     unityInstance.logo.className = "logo " + unityInstance.Module.splashScreenStyle;
     unityInstance.container.appendChild(unityInstance.logo);
   }
-  if (!unityInstance.progress) {
+  if (!unityInstance.progress) {    
     unityInstance.progress = document.createElement("div");
     unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
     unityInstance.progress.empty = document.createElement("div");
@@ -20,26 +20,5 @@ function UnityProgress(unityInstance, progress) {
   unityInstance.progress.full.style.width = (100 * progress) + "%";
   unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
   if (progress == 1)
-  {
     unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
-    var ele = document.getElementById("info");
-    ele.style.display = "block";
-  }
-
-}
-
-function click_btn()
-{
-  var ele = document.getElementById("info");
-  ele.style.display = "none";
-  unityInstance.SetFullscreen(1);
-}
-
-function keyisdown()
-{
- if (event.keyCode == 13){
-   var ele = document.getElementById("info");
-   ele.style.display = "none";
-   unityInstance.SetFullscreen(1);
- }
 }
